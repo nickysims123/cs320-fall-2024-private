06ad6d2aefcc7a6c4fae7d53b3e08bbe72088f24
Nicholas Sima <nick.sima@outlook.com>
2024-10-10 00:00:24 -0400

    progressed 04

--- assigns/assign05/lib/assign05_02.ml
+++ assigns/assign05/lib/assign05_02.ml
@@ -5,3 +5,12 @@ CS320 Fall 2024 A1
 
 Continuation Style Passing
 *)
+
+type 'a tree = 
+  | Leaf
+  | Node of 'a * 'a tree * 'a tree
+  
+let sum_tr tree = 
+  match tree with
+  | Leaf -> 0
+  | _ -> 0
