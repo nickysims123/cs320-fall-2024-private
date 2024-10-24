bd4e4cf9d663a8c5b2707a64ecdd69856e6dbce2
Nicholas Sima <nick.sima@outlook.com>
2024-10-24 13:37:15 -0400

    Finished 03?

--- assigns/assign06/lib/assign06_03.ml
+++ assigns/assign06/lib/assign06_03.ml
@@ -8,6 +8,21 @@ A Type Checker
 
 open Utils
 
-let type_of exp =
+let rec type_of exp =
   match exp with
-  | _ -> Some TInt
+  | Num(_) -> Some TInt
+  | Add(exp1, exp2) -> (
+                        match type_of exp1, type_of exp2 with
+                        | Some TInt, Some TInt -> Some TInt 
+                        | _ -> None
+                        )
+  | Lt(exp1, exp2) -> (
+                      match type_of exp1, type_of exp2 with 
+                      | Some TInt, Some TInt -> Some TBool
+                      | _ -> None
+                      )          
+  | Ite(exp1, exp2, exp3) -> (
+                              match type_of exp1, type_of exp2, type_of exp3 with
+                              | Some TBool, ty1, ty2 -> if ty1 = ty2 then ty1 else None
+                              | _ -> None
+                              )           
