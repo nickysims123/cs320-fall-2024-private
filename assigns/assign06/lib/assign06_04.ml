e533a4447a3f6ea110b0412be62248c9f17abb84
Nicholas Sima <nick.sima@outlook.com>
2024-10-24 21:25:30 -0400

    Finished 04

--- assigns/assign06/lib/assign06_04.ml
+++ assigns/assign06/lib/assign06_04.ml
@@ -8,4 +8,21 @@ An Evaluator
 
 open Utils
 
-let eval _ = (VNum(3))
+let rec eval exp = 
+  match exp with 
+  | Num(value) -> VNum value
+  | Add(exp1, exp2) -> (
+                      match (eval exp1), (eval exp2) with 
+                      | VNum val1, VNum val2 -> VNum (val1 + val2)
+                      | _ -> raise Not_found
+                      )
+  | Lt(exp1, exp2) -> (
+                      match (eval exp1), (eval exp2) with
+                      | VNum val1, VNum val2 -> VBool (val1 < val2)
+                      | _ -> raise Not_found
+                      )
+  | Ite(exp1, exp2, exp3) -> (
+                              match (eval exp1) with
+                              | VBool check -> if check then eval exp2 else eval exp3
+                              | _ -> raise Not_found
+                              )
